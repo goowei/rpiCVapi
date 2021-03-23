7385fbcbb2df7c381fc954b4991bc020078558a0
goowei <williamc129@gmail.com>
2021-03-23 19:23:47 +0800

    20210323 get context data with variable at the html side

---
+++ face_detection/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import JsonResponse
+import datetime
+
+
+# Create your views here.
+class FaceDetection(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        # working with variables in templates
+        context = super().get_context_data(**kwargs)
+        context["detectResult"] = datetime.datetime.now().strftime("%Y/%m/%d, %a, %I:%M %p")
+
+        return context
