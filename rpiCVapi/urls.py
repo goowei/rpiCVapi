647673a0f84d6349f16f834a76cc45dfda44ce1e
goowei <williamc129@gmail.com>
2021-04-01 19:47:05 +0800

    add more comment

--- rpiCVapi/urls.py
+++ rpiCVapi/urls.py
@@ -19,7 +19,9 @@ from face_detection.views import FaceDetection
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #
+    # 目前urls.py里path和re_path都只能指向视图view里的一个函数或方法，
+    # 而不能指向一个基于类的视图(Class Based View)。Django提供了一个额外as_view()方法，
+    # 可以将一个类伪装成方法。这点在当你使用Django在带的view类或自定义的类时候非常重要
     path('',FaceDetection.as_view(),name='home'),
     # call a method in the view.py
     path('facedetection/',FaceDetection.detect,name='detect')
